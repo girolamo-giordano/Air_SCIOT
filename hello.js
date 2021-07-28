9149d2954fe0d1c2e8dd0f26f5812da1e9e51483
Girolamo Giordano <56109364+girolamo-giordano@users.noreply.github.com>
2021-07-29 01:15:55 +0200

    cambiamenti iniziali

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ const bot = new Telegraf('1769775754:AAHZ3u_eR7-Bw0aruHLXpUYLGRqtFvmeKX8');
 
 bot.command('start', (ctx) => {
     console.log(ctx.from.first_name);
-    ctx.reply("Benvenuto b"+ctx.from.username);
+    ctx.reply("Benvenuto "+ctx.from.username);
 })
 
 bot.launch()
