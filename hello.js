06b0a70cd4bc8b58cb35420be48b3ac07395abb9
Girolamo Giordano <56109364+girolamo-giordano@users.noreply.github.com>
2021-07-28 23:47:35 +0200

    First commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+const {Telegraf} = require('telegraf');
+
+const bot = new Telegraf('1769775754:AAHZ3u_eR7-Bw0aruHLXpUYLGRqtFvmeKX8');
+
+bot.command('start', (ctx) => {
+    console.log(ctx.from.first_name);
+    ctx.reply("Benvenuto "+ctx.from.username);
+})
+
+bot.launch()
